c7a3cb61b4d8b94abbe20b22ed50047f0cc1ba24
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-08 11:22:42 +0800

    更新

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import serveIndex from "serve-index";
+
+const web_port =3031;
+const app = express();
+
+// routes
+
+app.use(express.static("public"));
+app.use("/",serveIndex("public", { icons: true}));
+
+app.listen(web_port, () => {
+    console.log(`伺服器啟動於通訊埠：${web_port}`);
+});
+
