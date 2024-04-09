955abd191691c3a1c6677e5542ce532accaaaf93
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-09 15:27:42 +0800

    編碼

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import express from "express";
 import serveIndex from "serve-index";
 
-const web_port =3031;
+const web_port = 3031;
 const app = express();
 
 // routes
 
-app.use(express.static("public"));
-app.use("/",serveIndex("public", { icons: true}));
+app.get("/try-sse", (req, res) => {
+  let id = 30;
 
-app.listen(web_port, () => {
-    console.log(`伺服器啟動於通訊埠：${web_port}`);
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream; charset=utf-8",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+
+  setInterval(() => {
+    const now = new Date();
+    res.write(`id: ${id++}\n`);
+    res.write(`data: ${ now.toLocaleString()}\n\n`);
+  }, 2000);
 });
 
+app.use(express.static("public"));
+app.use("/", serveIndex("public", { icons: true }));
+
+app.listen(web_port, () => {
+  console.log(`伺服器啟動於通訊埠: ${web_port}`);
+});
