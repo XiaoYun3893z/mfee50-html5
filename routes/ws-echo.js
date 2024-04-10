7c7c6d4bdee68d8e7ad6d509d26defc08317efb4
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-10 12:02:48 +0800

    ws-echo 前端測試(req.url)

--- routes/ws-echo.js
+++ routes/ws-echo.js
@@ -1,14 +1,15 @@
 import { WebSocketServer } from "ws";
 
-
-const wsServer =new WebSocketServer({ port: 3080});
+const wsServer = new WebSocketServer({ port: 3080 });
 
 wsServer.on("connection", (ws, req) => {
-    const ip = req.socket.remoteAddress;
+  const ip = req.socket.remoteAddress;
 
-    ws.on("message", (message) => {
-        ws.send(message.toString());
-    });
+  ws.on("message", (message) => {
+    ws.send(message.toString());
+  });
 
-    ws.send(`連線了, ${ip}, 連線數${wsServer.clients.size}`);
+  ws.send(`連線了, ${ip}, 連線數${wsServer.clients.size}, ${req.url}`);
 });
+
+export default wsServer;
